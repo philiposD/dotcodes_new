619ed22a4d96549696d961f6954862327818d67b
philiposD <drontzasphilipos@gmail.com>
2021-12-04 18:19:18 +0100

    -- New changes will be done in dev branch Dynamic routes and reading from MD files, might be helpful for small code snippets

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { getSortedPostsData } from '../lib/posts'
 import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
+import Date from '../components/date'
 
 /* export default function Home() {
   return (
@@ -94,11 +95,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
